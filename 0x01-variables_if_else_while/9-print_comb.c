a4c2a375cb207e4a66cb680c51970e4ed3961dc6
iSLAm4522 <islammadian798@gmail.com>
2024-09-02 18:10:51 +0300

    Add 9-print_comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int ch = 48;
+
+while (ch <= 57)
+{
+	putchar(ch);
+	if (ch != '9')
+	putchar(',');
+	putchar(' ');
+	ch++;
+}
+putchar('\n');
+return (0);
+}
