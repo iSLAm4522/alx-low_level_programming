79ffdc566078c587142b1d3dfb8c7e6c9963a7de
iSLAm4522 <islammadian798@gmail.com>
2024-09-02 16:16:02 +0300

    Add 6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int ch = 48;
+
+while (ch <= 57)
+{
+	putchar(ch++);
+}
+putchar('\n');
+return (0);
+}
