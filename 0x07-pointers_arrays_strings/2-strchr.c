b5c40cfb4c15ff6f92d2f9ec5ddf9d6833267f60
iSLAm4522 <islammadian798@gmail.com>
2024-09-09 20:03:51 +0300

    Add 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stddef.h>
 /**
 * _strchr - Locates a character in a string
 * @s: The string to be searched
@@ -18,5 +17,5 @@ while (*(s + i))
 	return (s + i);
 	i++;
 }
-return (NULL);
+return (0);
 }
