2f694ead16777c46f60e5e13087f1365e47190fa
iSLAm4522 <islammadian798@gmail.com>
2024-09-09 15:33:01 +0300

    Add 101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_number - Prints an integer using only _putchar
+ * @n: The integer to be printed
+ *
+ * Description: This function prints an integer digit by digit, handling
+ * both positive and negative numbers.
+ */
+
+void print_number(int n)
+{
+int num;
+
+if (n < 0)
+{
+	_putchar('-');
+	num = -n;
+}
+else
+	num = n;
+if (num / 10)
+	print_number(num / 10);
+_putchar(num % 10 + '0');
+}
