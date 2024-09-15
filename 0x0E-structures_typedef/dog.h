a476cf8f4dbad3f0aedfa03901114d142de1dccb
iSLAm4522 <islammadian798@gmail.com>
2024-09-15 16:16:12 +0300

    Add dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Structure to store information about a dog
+ * @name: Pointer to a character string representing the dog's name
+ * @age: A float representing the dog's age
+ * @owner: Pointer to a character string representing the name of the
+ * dog's owner.
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif /* DOG_H */
