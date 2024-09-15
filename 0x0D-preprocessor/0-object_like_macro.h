588e5a2e1cacafa27bd96431285502961f286246
iSLAm4522 <islammadian798@gmail.com>
2024-09-15 11:15:58 +0300

    Add 0-object_like_macro.h

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef OBJECT_LIKE_MACRO_H
-#define OBJECT_LIKE_MACRO_H
+#ifndef _0_OBJECT_LIKE_MACRO_H
+#define _0_OBJECT_LIKE_MACRO_H
 
 #define SIZE 1024
 
-#endif /* OBJECT_LIKE_MACRO_H */
+#endif /* _0_OBJECT_LIKE_MACRO_H */
