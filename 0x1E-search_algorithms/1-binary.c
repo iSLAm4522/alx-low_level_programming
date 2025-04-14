7fa256fb674d98fc19cae24f806b27a6009b2a0b
iSLAm4522 <islammadian798@gmail.com>
2025-04-15 00:16:37 +0200

    Add 1-binary.c

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -12,7 +12,11 @@ void print_array(int *array, size_t size)
 	size_t i;
 
 	for (i = 0; i < size; i++)
-		printf("%d ", array[i]);
+	{
+		if (i > 0)
+			printf(", ");
+		printf("%d", array[i]);
+	}
 	printf("\n");
 }
 
